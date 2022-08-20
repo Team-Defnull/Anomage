40f17c170131c5d162c2fe5db6e77c0133a69b59
Kynel <kynel95@gmail.com>
2022-08-20 23:32:49 +0900

    #0 fe-server 초기세팅

---
+++ fe-server/README.md
@@ -0,0 +1,10 @@
+# FE server of Anomage
+
+## 사용 스택
+
+next.js typescript tailwind-css
+
+## 실행
+
+1. yarn
+2. yarn dev
---
+++ fe-server/pages/index.tsx
@@ -0,0 +1,7 @@
+import type { NextPage } from "next";
+
+const Home: NextPage = () => {
+  return <div className="w-20 h-20 bg-yellow-500" />;
+};
+
+export default Home;
